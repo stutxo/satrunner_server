c22790dfa8e8f618bd4218fce3525d0b0c236e14
stum0 <70952638+stum0@users.noreply.github.com>
2023-07-30 20:57:59 +0100

    clean up play again

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ async fn main() {
     let value: Value = serde_json::from_str(&api_key_json).unwrap();
     let api_key = value["ZBD_API_KEY"].as_str().unwrap().to_string();
     let zebedee_client = ZebedeeClient::new().apikey(api_key).build();
-    // let zebedee_client = ZebedeeClient::new().apikey("test".to_string()).build();
+    //let zebedee_client = ZebedeeClient::new().apikey("test".to_string()).build();
 
     let rng_seed = rand::thread_rng().gen::<u64>();
 
